cb0015d2550c680e13132f623407cf1bfb4a7cd7
Mykhailo Chui <mihach0493@gmail>
2020-09-17 19:11:49 +0200

    test

---
+++ js/main.js
@@ -0,0 +1,85 @@
+
+
+
+document.querySelector("#button").addEventListener('click', function () {
+    var name = document.getElementById("name");
+    var surname = document.getElementById("surname");
+    var phone = document.getElementById("phone");
+    var container = document.getElementById("container");
+
+    container.textContent = ("name.value", "surname.value", "phone.value");
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let myHeading = document.querySelector('button');
+
+// let buttonAdd = document.querySelector('#name');
+// let buttonAdd = document.querySelector('#surname');
+// let pText = document.querySelector('#Dane');
+
+// buttonAdd.onclick = function () {
+
+//     pText.textContent = "test text";
+
+
+    // var ime = document.getElementById("#name");
+
+    // var nazwisko = document.getElementById("#surname");
+
+    // var handy = document.getElementById("#phone");
+    // document.getElementById("Wyświetl Dane").innerHTML;
+
+    // let myHeading = document.querySelector('button')
+
+
+    // let pText = document.querySelector('.tekst');
+
+
+
+
+
+
+
+
+
+
+    // var ime = document.getElementById("#name");
+
+    // var nazwisko = document.getElementById("#surname");
+
+    // var handy = document.getElementById("#phone");
+
+
+
+
